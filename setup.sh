49c2100e8827e46f9f36349fd694bebd7fb67655
Alykhan Kanji <github@alykhan.com>
2013-06-02 02:59:13 -0400

    Add git-completion.bash

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ ln -s $(pwd)/inputrc ~/.inputrc
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/gvimrc ~/.gvimrc
 ln -s $(pwd)/gitconfig ~/.gitconfig
+ln -s $(pwd)/git-completion.bash ~/.git-completion.bash
