03f4f5251bd1edbb41da8315ddbe0973ae5a8d1d
Alykhan Kanji <github@alykhan.com>
2013-06-02 02:39:05 -0400

    Add setup.sh, which creates home directory symlinks

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s $(pwd)/bash_profile ~/.bash_profile
+ln -s $(pwd)/inputrc ~/.inputrc
+ln -s $(pwd)/vimrc ~/.vimrc
+ln -s $(pwd)/gvimrc ~/.gvimrc
+ln -s $(pwd)/gitconfig ~/.gitconfig
