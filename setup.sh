819e7b384c711f9b2c6d26485b3a2575a0b76852
Alykhan Kanji <github@alykhan.com>
2013-09-05 14:11:58 -0400

    Prepare .vim directory in setup.sh

--- setup.sh
+++ setup.sh
@@ -7,4 +7,10 @@ ln -s $(pwd)/gvimrc ~/.gvimrc
 ln -s $(pwd)/gitconfig ~/.gitconfig
 ln -s $(pwd)/gitignore ~/.gitignore
 ln -s $(pwd)/git-completion.bash ~/.git-completion.bash
+
+git submodule init
+git submodule update
+mkdir -p ~/.vim/bundle/
+ln -s $(pwd)/vim-pathogen/autoload/ ~/.vim/autoload
+ln -s $(pwd)/vim-colors-solarized/ ~/.vim/bundle/vim-colors-solarized
 ln -s $(pwd)/ftplugin/ ~/.vim/ftplugin
