bec5efc9f7b20e5c987be922fe0ebd7c4dca7c57
Alykhan Kanji <github@alykhan.com>
2013-09-06 15:57:40 -0400

    Install vim-commentary on setup

--- setup.sh
+++ setup.sh
@@ -13,4 +13,5 @@ git submodule update
 mkdir -p ~/.vim/bundle/
 ln -s $(pwd)/vim-pathogen/autoload/ ~/.vim/autoload
 ln -s $(pwd)/vim-colors-solarized/ ~/.vim/bundle/vim-colors-solarized
+ln -s $(pwd)/vim-commentary/ ~/.vim/bundle/vim-commentary
 ln -s $(pwd)/ftplugin/ ~/.vim/ftplugin
