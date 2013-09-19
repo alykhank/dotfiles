2dd2a7271d55340bd895ce88d991bd0cbb10dea5
Alykhan Kanji <github@alykhan.com>
2013-09-19 02:28:46 -0400

    Ensure vim plugins are installed via vundle

--- setup.sh
+++ setup.sh
@@ -13,5 +13,6 @@ git submodule update
 mkdir -p ~/.vim/bundle/
 ln -s $(pwd)/vundle/ ~/.vim/bundle/vundle
 ln -s $(pwd)/ftplugin/ ~/.vim/ftplugin
+vim +BundleInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 ./install.sh --clang-completer
